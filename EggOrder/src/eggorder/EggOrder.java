138a23874fea11c0d4a791c17bfc6b7ff0d2f51d
Raph <30099423@tafe.wa.edu.au>
2025-11-04 15:00:33 +0800

    Formatting done

---
+++ EggOrder/src/eggorder/EggOrder.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eggorder;
+
+/**
+ *
+ * @author 30099423
+ */
+public class EggOrder {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        double perDozen = 7.25;
+        double each = 0.75;
+        
+        int eggNo = 26;
+        int cartons = eggNo/12;
+        int extras = eggNo%12;
+        
+        double total = cartons * perDozen + extras * each;
+        
+        System.out.printf("You ordered %d eggs.%n",eggNo);
+        String explanation = "That is %d dozen eggs at $%.2f per dozen and "
+                + "%d additional eggs at $%.2f for a total of $%.2f.%n";
+        System.out.printf(explanation, cartons, perDozen, extras, each, total);
+    }
+    
+}
