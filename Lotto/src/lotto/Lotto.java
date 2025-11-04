b9859e40cae8226236d8759cd0719d342bca6135
Raph <30099423@tafe.wa.edu.au>
2025-11-04 15:02:31 +0800

    Cleaned up formatting

--- Lotto/src/lotto/Lotto.java
+++ Lotto/src/lotto/Lotto.java
@@ -1,6 +1,6 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Name: Koi
+ * Description: a lottery class that generates a 
  */
 package lotto;
 
